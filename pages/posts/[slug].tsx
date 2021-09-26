ea970b0f1f041ff67fd6bcb88624c0a2c69eeece
Sang Le <lengobaosang@gmail.com>
2021-09-26 17:59:58 +0700

    [Done] setup header and footer

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -49,7 +49,7 @@ const Post = ({ post, morePosts, preview }: Props) => {
           <>
             <article className="mb-32">
               <Head>
-                <title>{post.title} | Next.js Blog Example with</title>
+                <title>{post.title} | Sang Le Tech Blog</title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
               <PostHeader
